09e0d481df07e25408560332398ec92e2d85cafe
Kireikin <kireikinav@yandex.ru>
2024-07-19 22:27:54 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -1,6 +1,8 @@
 immutable_var = ('Кортеж', 12345, True, [1, 2, True], [4, 'Text'], [7, False])
-print(immutable_var)
+print(immutable_var) # выводим кортеж тип 'tuple'
+#print(type(immutable_var)) # проверка
 # immutable_var[0] = 1  - TypeError: 'tuple' object does not support item assignment
-mutable_list = immutable_var[3:]
-mutable_list[1][1] = "Modified"
+mutable_list = list(immutable_var[3:]) # присваеваем преобразованный в список 'list'
+mutable_list[1][1] = "Modified Список"
 print(mutable_list)
+#print(type(mutable_list)) # проверка
