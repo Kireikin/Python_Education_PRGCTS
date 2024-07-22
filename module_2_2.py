bad41155bb60ec2d6c1c5fe2e60aa219d0df68a7
Kireikin <kireikinav@yandex.ru>
2024-07-22 19:19:16 +0300

    Add files via upload
    поправил... нужно делать пораньше

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,9 @@
 first = int(input('Введите первое число:'))
 second = int(input('Введите второе число:'))
 third = int(input('Введите третье число:'))
-if (int(first / second) == 1) and (int(second / third) == 1):  # условие если все равны
+if (first == second) and (second == third):  # условие если все равны
     print('Одинаковых чисел :', 3)
-elif (int(first / second) == 1) or (int(second / third) == 1) or (int(first / third) == 1):  # условие если два равны
+elif (first == second) or (second == third) or (first == third):  # условие если два равны
     print('Одинаковых чисел :', 2)
 else:
     print('Одинаковых чисел :', 0)
