8572e6c0c8c2e6e7918edd1f4295ec059982e0a4
Kireikin <kireikinav@yandex.ru>
2024-07-23 19:33:39 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -1,22 +1,24 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 1, 2, 3, 4]
+# обьявляю пустые списки
 primes = []
 not_primes = []
-is_prime = False
 for i in range(len(numbers)):
-    for j in (2,3,5,7):
-        if (numbers[i] % j == 0) and (numbers[i] != j):
-            is_prime = False
-            break
+    is_prime = True
+    # Исключаю единицу в значениях - пропускаю её проверку
+    if numbers[i] != 1:
+        # организую цикл проверки деления на 2 с шагом 1 до числа в numbers[i] - 1
+        for j in range(2,numbers[i]-1):
+            # если найден один из множителей
+            if (numbers[i] % j == 0):
+                is_prime = False
+                break
+            else:
+                is_prime = True
+        # заполняем списки по флагу is_prime
+        if  is_prime:
+            primes.append(numbers[i])
         else:
-            is_prime = True
-            continue
-    # Исключаю единицу
-    if numbers[i] == 1:
-        ()
-    elif is_prime:
-        primes.append(numbers[i])
-    else:
-        not_primes.append(numbers[i])
+            not_primes.append(numbers[i])
 # Результат:
 print(primes)
 print(not_primes)
