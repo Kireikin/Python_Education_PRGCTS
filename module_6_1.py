453339c4d12c889309e4eae0b6993f4ecfcb41df
Kireikin <kireikinav@yandex.ru>
2024-08-28 20:05:27 +0300

    Add files via upload

--- module_6_1.py
+++ module_6_1.py
@@ -5,15 +5,6 @@ class Animal:
     def __init__(self, name):
         self.name = name  # название животного
 
-
-class Plant:
-    edible = False  # не съедобный
-
-    def __init__(self, name):
-        self.name = name  # индивидуальное название каждого растения
-
-
-class Mammal(Animal):
     def eat(self, food):
         if food.edible:
             print(f'{self.name} съел {food.name}')
@@ -23,14 +14,19 @@ class Mammal(Animal):
             self.alive = False
 
 
+class Plant:
+    edible = False  # не съедобный
+
+    def __init__(self, name):
+        self.name = name  # индивидуальное название каждого растения
+
+
+class Mammal(Animal):
+    pass
+
+
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
-            self.alive = False
+    pass
 
 
 class Flower(Plant):
