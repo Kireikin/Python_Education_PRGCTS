cae24485e93354e91d28cfe85518ef76fd157727
Kireikin <kireikinav@yandex.ru>
2024-09-24 22:40:32 +0300

    Add files via upload

--- module_9_4.py
+++ module_9_4.py
@@ -1,25 +1,50 @@
 """
 "Создание функций на лету"
 """
+from random import choice
+
+
 # Lambda-функция:
-# first = 'Мама мыла раму'
-# second = 'Рамена мало было'
-#
-# result = list(map(lambda x, y: x == y, first, second))
-# """
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+result = list(map(lambda x, y: x == y, first, second))
+print(result)
 # Результатом должен быть список совпадения букв в той же позиции:
 # [False, True, True, False, False, False, False, False, True, False, False, False, False, False]
 # Где True - совпало, False - не совпало.
-# """
-# print(result)
+
 
 # Замыкание:
+
 def get_advanced_writer(file_name):
     def write_everything(*data_set):
-        write()
-
-    return write_everything(*data_set)
+        with open(file_name, 'w', encoding='utf-8') as file:
+            for elem in data_set:
+                file.write(str(elem) + '\n')
+    return write_everything
 
 
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+# Результатом должна быть запись в файле 'example.txt':
+# Это строчка
+# ['А', 'это', 'уже', 'число', 5, 'в', 'списке']
+
+# Метод __call__:
+
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
+
+# Результат - случайный:)
