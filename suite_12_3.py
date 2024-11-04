e86af4b02ce585ae80cf438ea5273dd03d284991
Kireikin <kireikinav@yandex.ru>
2024-11-04 23:18:30 +0300

    Задача "Заморозка кейсов"

--- suite_12_3.py
+++ suite_12_3.py
@@ -10,9 +10,10 @@ import tests_12_3
 
 
 test_mod_12_3 = unittest.TestSuite()
-test_mod_12_3.addTest(unittest.TestLoader().loadTestsFromTestCase(module_12_1.RunnerTest))
+# test_mod_12_3.addTest(unittest.TestLoader().loadTestsFromTestCase(module_12_1.RunnerTest))
 # test_mod_12_3.addTest(unittest.TestLoader().loadTestsFromTestCase(module_12_2.TournamentTest))
-test_mod_12_3.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.Test12M3(True)))
+test_mod_12_3.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.RunnerTest))
+test_mod_12_3.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.TournamentTest))
 
 
 
