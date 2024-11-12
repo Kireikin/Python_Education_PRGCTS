f5e5dce8a44927bfb5adcb5535ac671cd134e4d8
Kireikin <kireikinav@yandex.ru>
2024-11-12 07:27:05 +0300

    Add files via upload

--- module_13_4.py
+++ module_13_4.py
@@ -26,24 +26,29 @@ async def set_age(message):
 
 
 @dp.message_handler(state=UserState.age)
-async def set_growth(message):
+async def set_growth(message, state):
+    await state.update_data(age=int(message.text))
     await message.answer('Введите свой рост (см):')
     await UserState.growth.set()
 
 
 @dp.message_handler(state=UserState.growth)
-async def set_weight(message):
+async def set_weight(message, state):
+    await state.update_data(growth=int(message.text))
     await message.answer('Введите свой вес (кг):')
     await UserState.weight.set()
 
 
 @dp.message_handler(state=UserState.weight)
 async def send_calories(message, state):
-    await state.update_data()
+    await state.update_data(weight=int(message.text))
     data = await state.get_data()
     pprint(data)
-    calc = (10 * data['weight']) + (6.25 * data['growth']) - (5 * data['age']) + 5
+    print(data.get('weight'))
+    a, b, c = data.values()
+    calc = (10 * a) + (6.25 * b) - (5 * c) + 5
     # calc = (10 * data['weight']) + (6.25 * data['growth']) - (5 * data['age']) - 161
+    # calc = (10 * a) + (6.25 * b) - (5 * c) - 161
     await message.answer(f"Ваша норма калорий:{calc}")
     await state.finish
 # Для мужчин: ПБМ = (10 × вес в кг) + (6, 25 × рост в см) − (5 × возраст в годах) + 5.
