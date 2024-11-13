4bf9417b5bd0de38efe55527747ce287e937a311
Kireikin <kireikinav@yandex.ru>
2024-11-13 23:04:03 +0300

    Add files via upload
    Домашнее задание по теме "Инлайн клавиатуры".
    Цель: научится создавать Inline клавиатуры и кнопки на них в Telegram-bot.

---
+++ module_13_6.py
@@ -0,0 +1,90 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()  # возраст, полных лет
+    growth = State()  # рост, см
+    weight = State()  # вес, кг
+
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)  # , one_time_keyboard=True
+button1 = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+kb.add(button1, button2)
+
+kib = InlineKeyboardMarkup()
+inline_button1 = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+inline_button2 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+kib.add(inline_button1, inline_button2)
+
+
+@dp.callback_query_handler(text=['calories'])
+async def set_age(call):
+    await call.message.answer('Введите свой возраст (полных лет):')
+    await call.answer()
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=int(message.text))
+    await message.answer('Введите свой рост (см):')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=int(message.text))
+    await message.answer('Введите свой вес (кг):')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=int(message.text))
+    data = await state.get_data()
+    weight = data.get('weight')
+    growth = data.get('growth')
+    age = data.get('age')
+    # c, b, a = data.values()
+    # calc = (10 * weight) + (6.25 * growth) - (5 * age) + 5
+    # calc = (10 * a) + (6.25 * b) - (5 * c) - 161
+    calc = (10 * weight) + (6.25 * growth) - (5 * age) - 161
+    await message.answer(f"Ваша норма калорий:{calc}")
+    await state.finish()
+# Для мужчин: ПБМ = (10 × вес в кг) + (6, 25 × рост в см) − (5 × возраст в годах) + 5.
+# Для женщин: ПБМ = (10 × вес в кг) + (6, 25 × рост в см) − (5 × возраст в годах) − 161.
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью", reply_markup=kb)
+
+
+@dp.message_handler(text=['Информация'])
+async def start(message):
+    await message.answer("Я бот помогающий твоему здоровью, расчитываю норму калорий на сутки по формуле Формула "
+                         "Миффлина-Сан Жеора ", reply_markup=kb)
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer('ПБМ = (10 × вес в кг) + (6, 25 × рост в см) − (5 × возраст в годах) − 161')
+    await call.answer()
+
+
+@dp.message_handler(text='Рассчитать')
+async def main_menu(message):
+    await message.answer('Выберите опцию:', reply_markup=kib)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
