2a83df92169cbefc1db6fdc0d2f8a4155e7755aa
Kireikin <kireikinav@yandex.ru>
2024-11-17 19:58:24 +0300

    добавлен счетчик выполнения по времени

--- module_13_1.py
+++ module_13_1.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import datetime
 
 
 async def start_strongman(name, power):
@@ -18,4 +19,8 @@ async def start_tournament():
     await task2
     await task3
 
+time_start = datetime.now()
 asyncio.run(start_tournament())
+time_end = datetime.now()
+time_res = time_end - time_start
+print('Время соревнования:', time_res)
