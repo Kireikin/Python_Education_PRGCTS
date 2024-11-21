d168037aa149054d19652d1ce09f07a7cdc2ce28
Kireikin <kireikinav@yandex.ru>
2024-11-22 00:07:40 +0300

    Задания модуля 14

---
+++ module_14/module_14_1.py
@@ -0,0 +1,71 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')  # setup DataBase
+cursor = connection.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+cursor.execute(" CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
+
+# выше указанное - это настройка БД (setup), ниже  - это работа с базой данных
+
+
+def read_baze(*arg):  # читает всю базу каждый раз когда и как нам нужно
+    cursor.execute(*arg)
+    users = cursor.fetchall()  # записываем базу или результат вышеуказанной операции в переменную
+    for user in users:  # выводим в консоль
+        print(user)
+    print()
+
+
+#  Добавление записи в БД:
+for i in range(10):  # этот код генерирует пользователей согласно заданию выполняем 1 раз:
+    cursor.execute(" INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (f"User{i + 1}", f"example{i + 1}@gmail.com", (i + 1) * 10, 1000)
+                   )
+print("Сгенерированные записи в БД:")
+read_baze("SELECT * FROM Users")  # смотрим всё
+
+
+# Обновите balance у каждой 2ой записи начиная с 1ой на 500
+id = 1
+while id <= int(10):
+    cursor.execute("UPDATE Users SET balance = ? WHERE id =? ",
+                   (500, id)
+                   )
+    id += 2
+
+print("Обновленный balance у каждой 2ой записи начиная с 1ой на 500 в БД:")
+read_baze("SELECT * FROM Users")  # смотрим всё
+
+# Удалите каждую 3ую запись в таблице начиная с 1ой:
+id = 1
+while id <= int(10):
+    cursor.execute(" DELETE FROM Users WHERE id =? ",
+                   (id,))
+    id += 3
+
+print("Удалили каждую 3ую запись в таблице начиная с 1ой:")
+read_baze("SELECT * FROM Users")  # смотрим всё
+
+
+print("возраст не равен 60:")
+cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
+# read_baze("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
+users = cursor.fetchall()
+for user in users:  # выводим в консоль
+    print(f"Имя:{user[0]} | Почта:{user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+print()
+
+cursor.execute("DELETE FROM Users")  # чистим базу ;) из-за лени коментить каждый раз код
+print("Записи удалены, база не должна содержать данные")
+read_baze("SELECT * FROM Users")
+
+connection.commit()
+connection.close()
