5d008dd1806a8dc2374ef19b3c698193e8da2a7c
Kireikin <kireikinav@yandex.ru>
2024-11-22 00:12:48 +0300

    Delete homework1.py

--- homework1.py
+++
@@ -1,6 +0,0 @@
-example = "homework1.py"
-print(example[0])                   ##  первый символ строки
-print(example[-1:])                 ##  последний символ строки
-print(example[(len(example)//2):])  ##  вторая половина строки
-print(example[::-1])                ##  слово наоборот
-print(example[1::2])                ##  каждый второй символ строки
