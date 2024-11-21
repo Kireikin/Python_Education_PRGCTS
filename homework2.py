d81cfd72482112a7ee0a78d5c7335bd4841badf3
Kireikin <kireikinav@yandex.ru>
2024-11-22 00:13:01 +0300

    Delete homework2.py

--- homework2.py
+++
@@ -1,8 +0,0 @@
-num_homeworks_finish = 12   # количество выполненных ДЗ
-time_lost_h = 1.5           # затрачено времени, час
-Course_name = "Python"      # Название курса
-print('Курс:', Course_name,
-      ', всего задач:', num_homeworks_finish,
-      ', затрачено часов:', time_lost_h,
-      ', среднее время выполнения ', (time_lost_h / num_homeworks_finish),
-      ' часа')
