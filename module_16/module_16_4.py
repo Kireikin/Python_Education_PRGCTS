d9109ee10681e1a65c8452cebf844cf7e147fd7c
Kireikin <kireikinav@yandex.ru>
2024-12-12 22:25:16 +0300

    Add files via upload
    Цель: научиться описывать и использовать Pydantic модель. Задача "Модель пользователя":

--- module_16/module_16_4.py
+++ module_16/module_16_4.py
@@ -5,20 +5,20 @@ from typing import Annotated
 app = FastAPI()  # Создаем экземпляр приложения FastAPI
 
 
-class Users(BaseModel):  # POST-запрос — добавление данных
+class User(BaseModel):  # POST-запрос — добавление данных
     id: int
-    user_id: int
     username: str
     age: int
 
 
 # БД пользователей (словарь):
-users = [{"id": 1, "name": "Имя", "age": 18}]
+users = []
 
 
 @app.get("/")  # Определение базового маршрута
 async def root():
-    return {"message": "Домашнее задание по теме 'CRUD Запросы': Get, Post, Put Delete."}
+    return {"message": "Домашнее задание по теме: 'Модели данных Pydantic'/n Цель: научиться описывать "
+                       "и использовать Pydantic модель."}
 
 
 @app.get("/user/admin")  # Определение входа администратора
@@ -27,17 +27,17 @@ async def get_admin():
 
 
 @app.get("/user/{user_id}")  # модифицируем согласно заданию на валидацию
-async def get_user_id(user_id: Annotated[int, Path(ge=1, le=100,
-                                                   title="User ID",
-                                                   description="Enter User ID, "
-                                                               "The ID must be a positive integer 1 to 100",
-                                                   example=5
-                                                   )]):
+async def log_in_from_user_id(user_id: Annotated[int, Path(ge=1, le=100,
+                                                           title="User ID",
+                                                           description="Enter User ID, "
+                                                                       "The ID must be a positive integer 1 to 100",
+                                                           example=5
+                                                           )]):
     return {"Вы вошли как пользователь №": user_id}
 
 
 @app.get("/user/{username}/{age}")
-async def get_user(
+async def check_user_data(
         username: Annotated[str, Path(min_length=5, max_length=20,
                                       regex="^[a-zA-Z0-9_-]+$",
                                       title="User ID",
@@ -56,12 +56,12 @@ async def get_user(
 
 
 @app.get("/users")
-async def get_users():
+async def show_me_all_users():
     return users
 
 
-@app.post("/user/{username}/{age}")  # Создать новую задачу
-async def post_user(username: str, age: int):
+@app.post("/user/{username}/{age}")  # регистрировать нового пользователя
+async def add_new_user(username: str, age: int):
     new_id = max(user["id"] for user in users) + 1 if users else 1
     new_user = {"id": new_id, "username": username, "age": {age}}
     users.append(new_user)
@@ -69,21 +69,21 @@ async def post_user(username: str, age: int):
 
 
 @app.put("/user/{user_id}/{username}/{age}")  # меняем имя и возраст (данные) пользователя (записи) по id
-async def update_user(user_id: int, username: str, age: int):
-    for user in users:
-        if user["id"] == user_id:
-            user["username"] = username
-            user["age"] = age
-            return user
-    raise HTTPException(status_code=404, detail="Задача не найдена")
+async def update_user_data(user_id: int, username: str, age: int):
+    for user_ in users:
+        if user_["id"] == user_id:
+            user_["username"] = username
+            user_["age"] = age
+            return users
+    raise HTTPException(status_code=404, detail="User was not found")
 
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: int):
+async def delete_user_from_id(user_id: int):
     for i, user in enumerate(users):
         if user["id"] == user_id:
             del users[i]
-            return {"detail": "Задача удалена"}
-    raise HTTPException(status_code=404, detail="Задача не найдена")
+            return {"detail": "Пользователь удален"}
+    raise HTTPException(status_code=404, detail="User was not found")
 
 # START COMMAND IN LOCAL TERMINAL: uvicorn module_16_4:app --reload
