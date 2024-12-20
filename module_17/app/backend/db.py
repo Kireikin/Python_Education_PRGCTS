85b32edca54c29c45fd56926a526bb60a284bd69
Kireikin <kireikinav@yandex.ru>
2024-12-20 20:44:52 +0300

    Update db.py

--- module_17/app/backend/db.py
+++ module_17/app/backend/db.py
@@ -1,1 +1,12 @@
+# from homework in /backend
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, relationship
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
+engine = create_engine("sqlite:///taskmanager.db", echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
