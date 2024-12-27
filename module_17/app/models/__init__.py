2244b7a0fb8dece0c6b208afd9d2a6aca6e20925
Kireikin <kireikinav@yandex.ru>
2024-12-27 21:59:35 +0300

    Delete module_17 directory
    lerning manage the githab sys

--- module_17/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user import User
-from .task import Task
-# один user ко многим task
-
-# одна категория ко многим продуктам
-# from .category_lection import Category
-# from .products_lection import Product
