319b8d7aefaaccce0a9f4fceb1505f07415eb1e9
Алексей <kireikinav@yandex.ru>
2025-01-05 10:41:05 +0300

    Add files via upload

--- module_17/module_17_2/app/models/task.py
+++ module_17/module_17_2/app/models/task.py
@@ -1,1 +1,27 @@
-enter files
+# for homework in /models
+from app.backend.db import Base
+from sqlalchemy import Column, String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from app.models import user
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+    __table_args__ = {"extend_existing": True}  # многие ко многим?!
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    # Определяем отношение "многие к одному"
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=True, index=True)  # Связь с User
+
+    slug = Column(String, unique=True, index=True)
+    user = relationship("User", back_populates="tasks")
+
+
+if '__name__' == '__main__':
+    from sqlalchemy.schema import CreateTable  # для проверки сборки
+    print(CreateTable(Task.__table__))
+    pass
---
+++ module_17/module_17_2/app/models/user.py
@@ -0,0 +1,24 @@
+# for homework in /models
+from app.backend.db import Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from app.models import task
+
+
+class User(Base):
+    __tablename__ = "users"  # Имя таблицы в базе данных
+    __table_args__ = {"keep_existing": True} # один ко многим?!
+    id = Column(Integer, primary_key=True, index=True)  # Уникальный идентификатор
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)  # Человекочитаемый URL
+    tasks = relationship("Task", back_populates="user")
+    # user - объект связи с таблицей с таблицей User, где back_populates='tasks':
+    #  tasks = relationship("Task", back_populates="user")  # один user ко многим task
+
+
+from sqlalchemy.schema import CreateTable  # для проверки сборки
+print(CreateTable(User.__table__))  # проверяем сборку
+pass
